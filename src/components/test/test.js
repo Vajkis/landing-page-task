21831ecd289d0d4a8aee5ece54f563c1ff01edb5
Vajkis <vajkis1@gmail.com>
2025-06-04 23:13:33 +0300

    init

---
+++ src/components/test/test.js
@@ -0,0 +1,9 @@
+class TestComponent extends HTMLElement {
+  async connectedCallback() {
+    const response = await fetch('./components/test/test.html');
+    const template = await response.text();
+    this.innerHTML = template;
+  }
+}
+
+customElements.define('test-comp', TestComponent);
